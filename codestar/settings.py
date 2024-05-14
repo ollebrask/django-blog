2a746211b7bf85dbb9e74fc648fd196017c21609
Olle Brask <ollstah@gmail.com>
2024-05-14 12:08:33 +0000

    generic post view

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-ollebrask-djangoblog-3tr7l56wzip.ws-eu111.gitpod.io','.herokuapp.com']
 
